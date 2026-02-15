3aca212f51987e9d323141dabf0d735331d678e8
Sushant <69687013+TheMaster@users.noreply.github.com>
2026-02-15 22:03:41 +0530

    Solution to question 3

---
+++ question3.c
@@ -0,0 +1,30 @@
+// OS assignment 1 : question 3
+// grep line number extractor
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]) {
+    char cmd[2048];
+
+    // extract search term and file names from argv
+    char *searchTerm = argv[1];
+    char fileNames[1024];
+    int pos = 0;
+    for (int i = 2; i < argc; i++) {
+        int n = strlen(argv[i]);
+        for (int j = 0; j < n; j++) fileNames[pos++] = argv[i][j];
+        fileNames[pos++] = ' ';
+    }
+
+    // build the unix command
+    // search for searchTerm in fileNames, cut after : to get numbers and sort numbers to keep unique only
+    snprintf(cmd, 2048, "grep -n %s %s | cut -d: -f2 | sort -u -n", searchTerm, fileNames);
+    
+    // execute
+    execlp("sh", "sh", "-c", cmd, (char*) NULL);
+
+    return 0;
+}
